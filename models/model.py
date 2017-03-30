260849e35a843c9d8893562bd45e9c37176028df
prlz77 <pau.rodri1@gmail.com>
2017-03-30 18:38:48 +0200

    Docs and comments.

--- models/model.py
+++ models/model.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+""" 
+Creates a ResNeXt Model as defined in:
+
+Xie, S., Girshick, R., Dollár, P., Tu, Z., & He, K. (2016). 
+Aggregated residual transformations for deep neural networks. 
+arXiv preprint arXiv:1611.05431.
+
+"""
+
+__author__ = "Pau Rodríguez López, ISELAB, CVC-UAB"
+__email__ = "pau.rodri1@gmail.com"
+
 from __future__ import division
 import torch.nn as nn
 import torch.nn.functional as F
@@ -5,7 +18,19 @@ from torch.nn import init
 
 
 class ResNeXtBottleneck(nn.Module):
+    """
+    RexNeXt bottleneck type C (https://github.com/facebookresearch/ResNeXt/blob/master/models/resnext.lua)
+    """
     def __init__(self, in_channels, out_channels, stride, cardinality, widen_factor):
+        """ Constructor
+
+        Args:
+            in_channels: input channel dimensionality
+            out_channels: output channel dimensionality
+            stride: conv stride. Replaces pooling layer.
+            cardinality: num of convolution groups.
+            widen_factor: factor to reduce the input dimensionality before convolution.
+        """
         super(ResNeXtBottleneck, self).__init__()
         D = cardinality * out_channels // widen_factor
         self.conv_reduce = nn.Conv2d(in_channels, D, kernel_size=1, stride=1, padding=0, bias=False)
@@ -31,7 +56,19 @@ class ResNeXtBottleneck(nn.Module):
 
 
 class CifarResNeXt(nn.Module):
+    """
+    ResNext optimized for the Cifar dataset, as specified in
+    https://arxiv.org/pdf/1611.05431.pdf
+    """
     def __init__(self, cardinality, depth, nlabels, widen_factor=4):
+        """ Constructor
+
+        Args:
+            cardinality: number of convolution groups.
+            depth: number of layers.
+            nlabels: number of classes
+            widen_factor: factor to adjust the channel dimensionality
+        """
         super(CifarResNeXt, self).__init__()
         self.cardinality = cardinality
         self.depth = depth
@@ -59,6 +96,17 @@ class CifarResNeXt(nn.Module):
                 self.state_dict()[key][...] = 0
 
     def block(self, name, in_channels, out_channels, pool_stride=2):
+        """ Stack n bottleneck modules where n is inferred from the depth of the network.
+
+        Args:
+            name: string name of the current block.
+            in_channels: number of input channels
+            out_channels: number of output channels
+            pool_stride: factor to reduce the spatial dimensionality in the first bottleneck of the block.
+
+        Returns: a Module consisting of n sequential bottlenecks.
+
+        """
         block = nn.Sequential()
         for bottleneck in range(self.block_depth):
             name_ = '%s_bottleneck_%d' % (name, bottleneck)
