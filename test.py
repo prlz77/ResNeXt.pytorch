41cd48cc4435dd89f58dafcfcf43d983400e13ce
Pau Rodriguez <prlz77@users.noreply.github.com>
2019-08-26 13:34:05 -0400

    Update test.py
    update test to reflect changes in train

--- test.py
+++ test.py
@@ -38,6 +38,7 @@ def get_args():
     # Architecture
     parser.add_argument('--depth', type=int, default=29, help='Model depth.')
     parser.add_argument('--cardinality', type=int, default=8, help='Model cardinality (group).')
+    parser.add_argument('--base_width', type=int, default=64, help='Number of channels in each group.')
     parser.add_argument('--widen_factor', type=int, default=4, help='Widen factor. 4 -> 64, 8 -> 128, ...')
     # Acceleration
     parser.add_argument('--ngpu', type=int, default=1, help='0 = CPU.')
@@ -69,10 +70,10 @@ def test():
                                               num_workers=args.prefetch, pin_memory=True)
 
     # initialize model and load from checkpoint
-    net = CifarResNeXt(args.cardinality, args.depth, nlabels, args.widen_factor)
+    net = CifarResNeXt(args.cardinality, args.depth, nlabels, args.base_width, args.widen_factor)
     loaded_state_dict = torch.load(args.load)
     temp = {}
-    for key, val in list(loaded_state_dict.iteritems()):
+    for key, val in list(loaded_state_dict.items()):
         # parsing keys for ignoring 'module.' in keys
         temp[key[7:]] = val
     loaded_state_dict = temp
